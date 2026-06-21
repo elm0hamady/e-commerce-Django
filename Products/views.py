becf6fdfadc02140bdea67ed6740b71460f69a75
elm0hamady <melmohamady95@gmail.com>
2026-06-21 05:50:02 +0300

    add media (media root , media url)

--- Products/views.py
+++ Products/views.py
@@ -3,5 +3,5 @@ from .models import Product
 # Create your views here.
 def products(request):
     pro = Product.objects.all()
-    pro_filter = {'pro':str(pro.count())}
+    pro_filter = {'pro':pro}
     return render(request,'Products/products.html',pro_filter)
